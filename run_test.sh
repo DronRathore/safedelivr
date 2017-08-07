5d3778f467e9aa5e6ce0315453db2482b7fbbac3
Dron Rathore <dron.rathore@gmail.com>
2017-08-08 03:02:32 +0530

    Add Travis

---
+++ run_test.sh
@@ -0,0 +1,7 @@
+#!bin/sh
+# Currently facing problem with direct go -test
+# hence this file
+GOPATH=`pwd` go get
+GOPATH=`pwd` go test ./src/helpers/validations.go ./src/helpers/validations_test.go
+GOPATH=`pwd` go test ./src/helpers/workers.go ./src/helpers/workers_test.go
+GOPATH=`pwd` go test ./src/helpers/webhook.go ./src/helpers/webhook_test.go
