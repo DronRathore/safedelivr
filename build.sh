ac1f8e2b5b5fe439bb05fd23912fcf781d908708
Dron Rathore <dron.rathore@gmail.com>
2017-08-07 20:26:57 +0530

    Fix build app name

--- build.sh
+++ build.sh
@@ -6,10 +6,14 @@ go build app.go
 mkdir -p ./tmp
 if [ ! -f ./tmp/supervisor.sock ]; then
 	supervisord
+	supervisorctl stop worker:*
+	supervisorctl start worker:*
+	supervisorctl stop go:*
+	supervisorctl start go:*
 	supervisorctl status
 else
 	supervisorctl stop worker:*
 	supervisorctl start worker:*
-	supervisorctl stop app:*
-	supervisorctl start app:*
+	supervisorctl stop go:*
+	supervisorctl start go:*
 fi
