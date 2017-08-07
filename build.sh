2b2f7f2ebc66d6c9ab423d7a0c2d73a3c8c422ff
Dron Rathore <dron.rathore@gmail.com>
2017-08-07 20:21:01 +0530

    Add build script

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+export GOPATH=`pwd`
+go get
+go build worker.go
+go build app.go
+mkdir -p ./tmp
+if [ ! -f /tmp/supervisor.sock ]; then
+	supervisord
+	supervisorctl status
+else
+	supervisorctl stop worker:*
+	supervisorctl start worker:*
+	supervisorctl stop app:*
+	supervisorctl start app:*
+fi
