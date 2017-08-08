b10a053671aab48e2df816ab1f3d790d9897d820
Dron Rathore <dron.rathore@gmail.com>
2017-08-08 22:34:33 +0530

    Update goexpress, no need to parse multipart form now (#8)

--- src/helpers/webhook_test.go
+++ src/helpers/webhook_test.go
@@ -5,18 +5,15 @@ import(
 )
 
 func TestIsMultipart(t *testing.T){
-  var boundary = "|;|;|"
-  if IsMultipart("lol; noboundary=kkk", &boundary) != false {
+  if IsMultipart("lol; noboundary=kkk") != false {
     t.Log("Not a multipart header")
     t.Fail()
   }
-  boundary = "acdefawoeoeoeo"
-  if IsMultipart("form-data; boundary=acdefawoeoeoeo", &boundary) != true {
+  if IsMultipart("form-data; boundary=acdefawoeoeoeo") != true {
     t.Log("Is a multipart header")
     t.Fail()
   }
-  boundary = "acdefawoeoeoeo"
-  if IsMultipart("form-data; boundaryies=acdefawoeoeoeo", &boundary) != false {
+  if IsMultipart("form-data; boundaryies=acdefawoeoeoeo") != false {
     t.Log("Is a multipart header")
     t.Fail()
   }
