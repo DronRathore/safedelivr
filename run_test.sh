3633100cc2b30ce4fdec560d4c2c22473af8face
Dron Rathore <dron.rathore@gmail.com>
2017-08-08 12:05:00 +0530

    desperate attempt++

--- run_test.sh
+++ run_test.sh
@@ -2,6 +2,4 @@
 # Currently facing problem with direct go -test
 # hence this file
 GOPATH=`pwd` go get
-GOPATH=`pwd` go test ./src/helpers/validations.go ./src/helpers/validations_test.go
-GOPATH=`pwd` go test ./src/helpers/workers.go ./src/helpers/workers_test.go
-GOPATH=`pwd` go test ./src/helpers/webhook.go ./src/helpers/webhook_test.go
+GOPATH=`pwd` go test -v ./src/helpers
